56f2dd34fafe33b95ceb01982088db5330ae7235
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-10-05 06:46:53 +0700

    Создана переменная для EditText

--- app/src/main/java/com/example/third/MainActivity.kt
+++ app/src/main/java/com/example/third/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.third
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button: Button = findViewById<Button>(R.id.button)
+        val inputtext1: EditText = findViewById<EditText>(R.id.editTextText)
     }
 }
